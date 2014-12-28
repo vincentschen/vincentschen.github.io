913ab529345107310fafcfe9a252bf1d37bd26ce
Vincent <vincentschen@yahoo.com>
2014-12-28 18:50:35 -0500

    [new] content containers

--- js/main.js
+++ js/main.js
@@ -4,9 +4,23 @@ $(document).ready(function(){
 		$('.overlay').addClass('open');
 	})
 
+	$('#projects').click(function(){
+		$('#projectsContainer').show();
+	})
+
+	$('#experience').click(function(){
+		$('#experienceContainer').show();
+	})
+
+	$('#coursework').click(function(){
+		$('#courseworkContainer').show();
+	})
+
+
 	$('.overlay').click(function(){
 		$(this).removeClass('open');
 		$('#projects, #experience, #coursework').removeClass('categoryActive');
+		$('#projectsContainer, #experienceContainer, #courseworkContainer').hide();
 	})
 
 });
