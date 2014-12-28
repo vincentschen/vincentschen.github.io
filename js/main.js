83a1d7a7058ba61ba41845f7905e1f6a9d798fd3
Vincent <vincentschen@yahoo.com>
2014-12-28 13:03:37 -0500

    [new] hover tooltips contacts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	$('ul.contactIcons li').tooltip();
+	$('#projects').click(function(){
+		$('.projects').show("slow");
+	})
+
+	$('#myName').click(function(){
+		$('.projects').hide("slow");
+	})
+
+	$( "#email" ).hover(function() {
+    	$( this ).append( $( "<h1> ***</h1>" ) );
+  	}, function() {
+    	$( this ).find( "h1:last" ).remove();
+  }
+);
+
+});
+
+
 });
