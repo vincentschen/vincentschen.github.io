f23cac7ffc93221fe054d7038bcac6bc3ef99855
Vincent <vincentschen@yahoo.com>
2014-12-27 22:45:37 -0500

    [new] working contact links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	//fade in contact information on click
-	$('#contact').click(function(){
-		$('.contact').fadeIn("fast");
-	});
-
-	//fade out contact information on click of another link 
-	$('#myName').click(function(){
-		$('.contact').fadeOut("fast");
-	});
+	$('ul.contactIcons li').tooltip();
 });
