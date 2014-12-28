1a23ce7e7079092611fc392b0e782d15e2a4ddf8
Vincent <vincentschen@yahoo.com>
2014-12-27 22:33:00 -0500

    [new] contact bar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+	//fade in contact information on click
+	$('#contact').click(function(){
+		$('.contact').fadeIn("fast");
+	});
 
+	//fade out contact information on click of another link 
+	$('#myName').click(function(){
+		$('.contact').fadeOut("fast");
+	});
+});
